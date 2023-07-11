3c9579b86672cea05da657dd7578b0e1e0c53d6b
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-07-11 18:59:19 -0500

    ++ Lang, Title & Re added _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import Head from 'next/head'
+import React from 'react'
+import { name } from 'package.json';
+
 import 'styles/globals.scss';
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>{ name }</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { name } from 'package.json';
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
