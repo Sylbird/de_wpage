b704dc2003180fcc5bd2adcd5220ea3c79710b57
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-07-25 17:55:54 -0500

    ++ declaration file to extend defaultTheme

--- components/StyledApp.tsx
+++ components/StyledApp.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
-import theme from 'styles/themes';
+import theme from 'styles/defaultTheme/theme';
 
 const StyledApp: FC = ({ children }) => (
   <>
---
+++ styles/defaultTheme/theme.ts
@@ -0,0 +1,9 @@
+import type { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  colors: {
+    primary: '#0070f3'
+  }
+};
+
+export default defaultTheme;
