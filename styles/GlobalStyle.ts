c543257c2e5922cc8d0853d91f79d58c380ef50c
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-08 18:00:32 -0500

    ++ Rules to GlobalStyle

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  *,
+  *::before,
+  *::after {
+    border: 0;
     box-sizing: border-box;
+    cursor: default;
+    font-variant-numeric: tabular-nums;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+    text-rendering: optimizeLegibility;
+    -webkit-touch-callout: none;
+    user-select: none;
   }
 `;
+
 export default GlobalStyle;
