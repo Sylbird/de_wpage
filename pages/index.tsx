326e32313a6dfa47a2d54ee257262796775309aa
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-12 19:53:05 -0500

    Removed unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Desktop from 'components/system/Desktop';
 import ProcessLoader from 'components/system/ProcessLoader';
 import { ProcessProvider } from 'contexts/process';
-import { getStartupProcesses } from 'contexts/process/directory';
 
 export default function Home() {
   return (
