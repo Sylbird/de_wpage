74324a76909f9c58f9d89a8cc7822dd56af35669
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-13 16:31:16 -0500

    Extracted ProcessesReducer

--- components/system/ProcessLoader.tsx
+++ components/system/ProcessLoader.tsx
@@ -1,14 +1,15 @@
 import RenderProcess from 'components/system/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
+import type { Process } from 'contexts/process/types';
+
+const ProcessesReducer = ([id, process]: [string, Process]) => (
+  <RenderProcess key={id} {...process} />
+);
 
 const ProcessLoader: FC = () => {
   return (
     <ProcessConsumer>
-      {({ processes }) =>
-        Object.entries(processes).map(([id, process]) => (
-          <RenderProcess key={id} {...process} />
-        ))
-      }
+      {({ processes }) => Object.entries(processes).map(ProcessesReducer)}
     </ProcessConsumer>
   );
 };
