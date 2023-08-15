1ea3ec1a3e73d72891646e9f6d78879449027651
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-14 22:58:37 -0500

    ++ Type for process context state

--- hooks/useProcessContextState.ts
+++ hooks/useProcessContextState.ts
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import type { ProcessContextState } from 'contexts/process/types';
+import type { ProcessContextState, Processes } from 'contexts/process/types';
 import { HelloWorld, Taskbar } from 'contexts/process/directory';
 
 const useProcessContextState = (): ProcessContextState => {
-  const [processes] = useState({ HelloWorld, Taskbar });
+  const [processes] = useState<Processes>({ HelloWorld, Taskbar });
 
   return { processes };
 };
