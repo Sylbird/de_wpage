e71c3464c2d11f741ea61706da3506eaa9b02d20
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-20 21:04:23 -0500

    Use file info Hook + getShortcut

--- utils/constants.ts
+++ utils/constants.ts
@@ -16,4 +16,30 @@ export const DEFAULT_LOCALE = 'en';
 
 export const DEFAULT_THEME: ThemeName = 'defaultTheme';
 
+export const TIFF_IMAGE_FORMATS = ['.cr2', '.dng', '.nef', '.tif', '.tiff'];
+
+export const IMAGE_FILE_EXTENSIONS = [
+  ...TIFF_IMAGE_FORMATS,
+  '.ani',
+  '.apng',
+  '.avif',
+  '.bmp',
+  '.cur',
+  '.gif',
+  '.ico',
+  '.jfif',
+  '.jif',
+  '.jpe',
+  '.jpeg',
+  '.jpg',
+  '.jxl',
+  '.pjp',
+  '.pjpeg',
+  '.png',
+  '.svg',
+  '.qoi',
+  '.webp',
+  '.xbm'
+];
+
 export const MILLISECONDS_IN_SECOND = 1000;
