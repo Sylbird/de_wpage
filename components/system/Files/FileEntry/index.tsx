495b7dbc0e2b7ef567707f548b08ea7c48199de2
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-08-29 17:28:53 -0500

    Custom DoubleClick handler

--- components/system/Files/FileEntry/index.tsx
+++ components/system/Files/FileEntry/index.tsx
@@ -3,6 +3,7 @@ import useFileInfo from 'hooks/useFileInfo';
 import StyledFileEntry from 'components/system/Files/FileEntry/StyledFileEntry';
 import { useCallback } from 'react';
 import Button from 'styles/custom/Button';
+import useDoubleClick from 'hooks/useDoubleClick';
 
 type FileEntryProps = {
   name: string;
@@ -12,11 +13,11 @@ type FileEntryProps = {
 const FileEntry = ({ name, path }: FileEntryProps): JSX.Element => {
   const { icon, pid } = useFileInfo(path);
   const { open } = useProcesses();
-  const onActivate = useCallback(() => open(pid), [open, pid]);
+  const onClick = useCallback(() => open(pid), [open, pid]);
 
   return (
     <StyledFileEntry>
-      <Button onClick={onActivate} onKeyDown={onActivate}>
+      <Button onClick={useDoubleClick(onClick)}>
         <figure>
           <img src={icon} alt={name} />
           <figcaption>{name}</figcaption>
