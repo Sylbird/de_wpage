87f167187bb2d66a3f873f8a52dd224d0cf81d1f
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-09-06 13:13:21 -0500

    Added draggable hook

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -5,6 +5,7 @@ import Titlebar from 'components/system/Window/Titlebar';
 import { Rnd } from 'react-rnd';
 import useResizable from 'hooks/useResizable';
 import rndDefaults from 'utils/rndDefaults';
+import useDraggable from 'hooks/useDraggable';
 
 const Window: FC<ProcessComponentProps> = ({ children, id }) => {
   const {
@@ -13,11 +14,15 @@ const Window: FC<ProcessComponentProps> = ({ children, id }) => {
     }
   } = useProcesses();
   const { height, width, updateSize } = useResizable(maximized);
+  const { x, y, updatePosition } = useDraggable(maximized);
+
   return (
     <Rnd
-      size={{ height, width }}
       enableResizing={!maximized}
+      onDragStop={updatePosition}
       onResizeStop={updateSize}
+      position={{ x, y }}
+      size={{ height, width }}
       {...rndDefaults}
     >
       <StyledWindow $minimized={minimized}>
