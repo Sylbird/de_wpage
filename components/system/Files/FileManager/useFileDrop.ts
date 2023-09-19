82668a952777c566bbdcb92f265fb2398a69c119
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-09-19 15:13:33 -0500

    Replaced getFiles with updateFiles to append new files

--- components/system/Files/FileManager/useFileDrop.ts
+++ components/system/Files/FileManager/useFileDrop.ts
@@ -11,7 +11,10 @@ type FileDrop = {
   onDrop: (event: React.DragEvent<HTMLElement>) => void;
 };
 
-const useFileDrop = (directory: string, getFiles: () => void): FileDrop => {
+const useFileDrop = (
+  directory: string,
+  updateFiles: (appendFile?: string) => void
+): FileDrop => {
   const { fs } = useFileSystem();
   const onDrop = useCallback(
     (event: React.DragEvent<HTMLElement>) => {
@@ -23,12 +26,12 @@ const useFileDrop = (directory: string, getFiles: () => void): FileDrop => {
         fs?.writeFile(
           `${directory}/${file.name}`,
           Buffer.from(new Uint8Array(target?.result as ArrayBuffer)),
-          getFiles
+          (error) => !error && updateFiles(file.name)
         );
 
       reader.readAsArrayBuffer(file);
     },
-    [directory, fs, getFiles]
+    [directory, fs, updateFiles]
   );
   return {
     onDragOver: haltDragEvent,
