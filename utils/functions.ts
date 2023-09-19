bff2333dbdeec09cad4ae617ec36f50bd885598d
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-09-19 14:51:36 -0500

    Compare extensions as lowerCase

--- utils/functions.ts
+++ utils/functions.ts
@@ -42,7 +42,7 @@ export const loadStyle = (href: string): Promise<Event> =>
 export const loadFiles = async (files: string[]): Promise<Event[]> =>
   Promise.all(
     files.reduce((filesToLoad: Promise<Event>[], file) => {
-      const ext = extname(file);
+      const ext = extname(file).toLowerCase();
 
       if (ext === '.css') filesToLoad.push(loadStyle(file));
       else if (ext === '.js') filesToLoad.push(loadScript(file));
