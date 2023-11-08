ab894fd35ac56b73e1f1df33eeefaae60717fd9e
Dvl <84147332+Sylbird@users.noreply.github.com>
2023-11-08 12:13:07 -0500

    Moved setWindowStates into onDragStop & onResizeStop

--- components/system/Window/RndWindow/useRnd.ts
+++ components/system/Window/RndWindow/useRnd.ts
@@ -13,29 +13,44 @@ const useRnd = (id: string, maximized = false): Props => {
       [id]: { autoSizing }
     }
   } = useProcesses();
-  const { windowStates: { [id]: windowState } = {} } = useSession();
+  const { windowStates: { [id]: windowState } = {}, setWindowStates } =
+    useSession();
   const { position: statePosition, size: stateSize } = windowState || {};
   const [position, setPosition] = useDraggable(maximized, statePosition);
   const [size, setSize] = useResizable(maximized, autoSizing, stateSize);
 
   const onDragStop = useCallback<DraggableEventHandler>(
-    (_event, { x: positionX, y: positionY }) =>
-      setPosition({ x: positionX, y: positionY }),
-    [setPosition]
+    (_event, { x, y }) => {
+      const newPosition = { x, y };
+
+      setPosition(newPosition);
+      setWindowStates((currentWindowStates) => ({
+        ...currentWindowStates,
+        [id]: {
+          ...currentWindowStates[id],
+          position: newPosition
+        }
+      }));
+    },
+    [setPosition, setWindowStates, id]
   );
 
   const onResizeStop = useCallback<RndResizeCallback>(
-    (
-      _event,
-      _direction,
-      { style: { height: elementHeight, width: elementWidth } },
-      _delta,
-      { x: positionX, y: positionY }
-    ) => {
-      setSize({ height: elementHeight, width: elementWidth });
-      setPosition({ x: positionX, y: positionY });
+    (_event, _direction, { style: { height, width } }, _delta, newPositon) => {
+      const newSize = { height: height, width: width };
+
+      setSize(newSize);
+      setPosition(newPositon);
+      setWindowStates((currentWindowStates) => ({
+        ...currentWindowStates,
+        [id]: {
+          ...currentWindowStates[id],
+          position: newPositon,
+          size: newSize
+        }
+      }));
     },
-    [setSize, setPosition]
+    [id, setSize, setPosition, setWindowStates]
   );
 
   return {
