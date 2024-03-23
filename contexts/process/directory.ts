c62b0e8e29804c5f5600b6c872a92b700ed29bf1
Dvl <84147332+Sylbird@users.noreply.github.com>
2024-03-23 16:32:37 -0500

    Default `hasWindow` to true

--- contexts/process/directory.ts
+++ contexts/process/directory.ts
@@ -4,14 +4,12 @@ import type { Processes } from 'contexts/process/types';
 const processDirectory: Processes = {
   FileExplorer: {
     Component: dynamic(() => import('components/apps/FileExplorer')),
-    hasWindow: true,
     icon: '/System/Icons/explorer.png',
     title: 'FileExplorer'
   },
   JSDOS: {
     autoSizing: true,
     Component: dynamic(() => import('components/apps/JSDOS')),
-    hasWindow: true,
     icon: '/System/Icons/JSDOS.png',
     lockAspectRatio: true,
     title: 'js-dos v7'
@@ -19,7 +17,6 @@ const processDirectory: Processes = {
   V86: {
     autoSizing: true,
     Component: dynamic(() => import('components/apps/V86')),
-    hasWindow: true,
     icon: '/System/Icons/v86.png',
     title: 'Virtual x86'
   }
