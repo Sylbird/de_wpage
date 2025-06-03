1a660dc94f81c487f5ef59b805279774038b6817
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-06-03 18:53:28 -0500

    Moved `ProcessProvider` Up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import StyledApp from 'components/pages/StyledApp';
 import Metadata from 'components/pages/Metadata';
-
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'contexts/session';
 import { FileSystemProvider } from 'contexts/fileSystem';
+import { ProcessProvider } from 'contexts/process';
 
 const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
   <FileSystemProvider>
     <Metadata />
     <SessionProvider>
-      <StyledApp>
-        <Component {...pageProps} />
-      </StyledApp>
+      <ProcessProvider>
+        <StyledApp>
+          <Component {...pageProps} />
+        </StyledApp>
+      </ProcessProvider>
     </SessionProvider>
   </FileSystemProvider>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,13 @@ import Desktop from 'components/system/Desktop';
 import AppsLoader from 'components/system/Apps/AppsLoader';
 import Taskbar from 'components/system/Taskbar';
 import FileManager from 'components/system/Files/FileManager';
-import { ProcessProvider } from 'contexts/process';
 
 const Home = (): React.ReactElement => (
-  <ProcessProvider>
-    <Desktop>
-      <FileManager directory="User/Public/Desktop" />
-      <Taskbar />
-      <AppsLoader />
-    </Desktop>
-  </ProcessProvider>
+  <Desktop>
+    <FileManager directory="User/Public/Desktop" />
+    <Taskbar />
+    <AppsLoader />
+  </Desktop>
 );
 
 export default Home;
