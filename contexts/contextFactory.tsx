2086afd8bd58699f01b52c82fc2049b04783c229
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-11 14:22:27 -0500

    Allow components in context factory

--- contexts/contextFactory.tsx
+++ contexts/contextFactory.tsx
@@ -2,7 +2,8 @@ import { createContext, useContext } from 'react';
 
 type ContextFactory = <T>(
   initialContextState: T,
-  useContextState: () => T
+  useContextState: () => T,
+  ContextComponent?: React.ComponentType
 ) => {
   Consumer: React.Consumer<T>;
   Provider: FC;
@@ -11,12 +12,16 @@ type ContextFactory = <T>(
 
 const contextFactory: ContextFactory = (
   initialContextState,
-  useContextState
+  useContextState,
+  ContextComponent
 ) => {
   const Context = createContext(initialContextState);
 
   const ProcessProvider: FC = ({ children }) => (
-    <Context.Provider value={useContextState()}>{children}</Context.Provider>
+    <Context.Provider value={useContextState()}>
+      {children}
+      {ContextComponent ? <ContextComponent /> : <></>}
+    </Context.Provider>
   );
 
   return {
